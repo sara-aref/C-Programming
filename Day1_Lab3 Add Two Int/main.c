76d21c38d4e4dc465314cd2677b83dfb2b935409
sara-aref <147546807+sara-aref@users.noreply.github.com>
2023-10-23 22:50:28 +0300

    Add files via upload

---
+++ Day1_Lab3 Add Two Int/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1 = 2;
+    int num2 = 6;
+    int sum = num1 + num2;
+    printf("The sum = %d + %d = %d\n",num1,num2,sum);
+    return 0;
+}
