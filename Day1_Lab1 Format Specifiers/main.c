b6088fc22ccca14e9e4c6c7211a540b2a6afb604
sara-aref <147546807+sara-aref@users.noreply.github.com>
2023-10-23 22:47:49 +0300

    Add files via upload

---
+++ Day1_Lab1 Format Specifiers/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1 = 10;
+    printf("integer = %d\n",num1);
+
+    float num2 = 20.5;
+    printf("floatnum = %f\n",num2);
+
+    char c = 'a';
+    printf("character = %c\n",c);
+
+    int Hexadecimal = 15;
+    printf("Hexadecimal = %x\n",Hexadecimal);
+
+    long Longnum = 15;
+    printf("Longnum = %ld\n",Longnum);
+
+    double doublenum = 5;
+    printf("doublenum = %lf\n",doublenum);
+
+
+    return 0;
+}
