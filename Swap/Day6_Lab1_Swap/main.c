db759aa5683a135bb365fe8e35a9158abddea7bc
sara-aref <147546807+sara-aref@users.noreply.github.com>
2023-10-24 20:47:29 +0300

    Add files via upload

---
+++ Swap/Day6_Lab1_Swap/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int *x,int *y);
+
+int main()
+{
+    int x = 0,y = 0;
+    printf("Enter first number: ");
+    scanf("%d",&x);
+    printf("Enter second number: ");
+    scanf("%d",&y);
+
+    swap(&x, &y);
+    printf("After swapping Number 1 & Number 2: %d & %d",x,y);
+
+    return 0;
+}
+
+int swap(int * x,int * y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
---
+++ Swap/Day6_Lab1_Swap3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int * x, int * y);
+
+int main()
+{
+    int x = 0, y = 0;
+    printf("Enter first number: ");
+    scanf("%d",&x);
+    printf("Enter second number: ");
+    scanf("%d",&y);
+
+    swap(&x, &y);
+    printf("After swapping Number 1 & Number 2: %d & %d",x,y);
+
+    return 0;
+}
+
+int swap(int * x, int * y)
+{
+    *x = *x ^ *y;
+    *y = *x ^ *y;
+    *x = *x ^ *y;
+}
