ef63eeb7cf42655303d373c26694c73246aa19a2
sara-aref <147546807+sara-aref@users.noreply.github.com>
2023-10-24 08:43:32 +0300

    Add files via upload

---
+++ Assignment1_18/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int size=0;
+    printf("Enter the size:\n");
+    scanf("%d",&size);
+
+    for(int i=0; i<size; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
