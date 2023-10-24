fa0593bdde9a4386c630c2f2f92fcec5c64da1be
sara-aref <147546807+sara-aref@users.noreply.github.com>
2023-10-24 08:38:33 +0300

    Add files via upload

---
+++ Assignment1_5/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c = 0;
+    printf("Enter the character: \n");
+    scanf("%c",&c);
+
+    printf("The ASCII of the character: %d\n",c);
+
+    return 0;
+}
